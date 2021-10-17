053d06086f69107311817ca85736d5e6e8b76fb5
Siva3v <siva3v@gmail.com>
2021-10-17 12:37:30 +0530

    01

--- app/src/main/java/com/hansilk/two/MainActivity.kt
+++ app/src/main/java/com/hansilk/two/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.hansilk.two
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hansilk.two.blocks.uploads.main.UploadsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, UploadsActivity::class.java))
+
     }
 }
